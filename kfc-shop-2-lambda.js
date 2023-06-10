4c5c51ea579974965f4137081fb9f217399fd36d
kohlidevops <100069489+kohlidevops@users.noreply.github.com>
2023-06-10 10:31:04 +0530

    Create kfc-shop-2-lambda.js

---
+++ kfc-shop-2-lambda.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) => {
+    console.log(JSON.stringify(event));
+    const order = JSON.parse(event.Records[0].body);
+    console.log('Received Order: ', order);
+    console.log('Kfc Shop #2 Making Burger... ...');
+    await sleep(5000);
+};
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
